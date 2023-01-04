773bb3fff7fbc0d8fc8616dbd50ce1cec16ca932
ElCreaturoM50S <113674920+ElCreaturoM50S@users.noreply.github.com>
2023-01-04 09:39:15 +0100

    Update

--- reactgaming/src/App.js
+++ reactgaming/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {StyleSheet, Text, View} from 'react-native';
 
-function App() {
+import StartGameScreen from './GUI/StartGameScreen';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StartGameScreen/>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
